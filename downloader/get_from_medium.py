4135bc4fc24b8d30309be49371498590f7e7899a
Nóra Szepes <nora.szepes@tenforce.com>
2017-05-21 16:24:52 +0200

    fetching posts from medium in JSON
    Arguments: username, limit (optional)

--- downloader/get_from_medium.py
+++ downloader/get_from_medium.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
-def test(arg1):
-    print(arg1)
+import requests
+
+# Get latest X posts o
+def get_latest_posts(user, limit):
+    """
+        Get latest {limit} posts from {user} in JSON format
+    """
+    url = "https://medium.com/@{0}/latest".format(user)
+
+    if limit:
+        params = {"limit": limit}
+    else:
+        params =  {}
+
+    headers = {'Accept': 'application/json'}
+
+    response = requests.get(url, params, headers=headers)
+    print(response.url)
+    print(response.text)
--- run.py
+++ run.py
@@ -5,9 +5,7 @@ import argparse
 
 parser = argparse.ArgumentParser(prog='run.py', description="Script to convert medium posts to markdown files")
 parser.add_argument('-u', metavar='USER', help="username", required=True)
-parser.add_argument('-l', metavar='LIMIT', help="Number of posts to retrieve")
+parser.add_argument('-l', metavar='LIMIT', help="Number of posts to retrieve, default is 10 by Medium")
 args = parser.parse_args()
 
-medium.test(args.u)
-if args.l:
-    print(args.l)
+medium.get_latest_posts(args.u, args.l)
