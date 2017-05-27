508f09dbc9b7e3ae5e27b52a4e67422109cbbc83
Nóra Szepes <nora.szepes@tenforce.com>
2017-05-27 13:10:57 +0200

    added argument types

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ import downloader.get_from_medium as medium
 import argparse
 
 parser = argparse.ArgumentParser(prog='run.py', description="Script to convert medium posts to markdown files")
-parser.add_argument('-u', metavar='USER', help="username", required=True)
-parser.add_argument('-l', metavar='LIMIT', help="Number of posts to retrieve, default is 10 by Medium")
+parser.add_argument('-u', metavar='USER', help="username", required=True, type=str)
+parser.add_argument('-l', metavar='LIMIT', help="Number of posts to retrieve. Integer, and the default is 10 by Medium. Zero will be swapped to one.", type=int)
 args = parser.parse_args()
 
 medium.get_latest_posts(args.u, args.l)
